5a21318160accea74dc0bc2bbd837a3b7f754227
Jakob Höper <jhoeper@bbaw.de>
2016-03-11 19:16:55 +0100

    Introduce BTSLemmaQueryRequest class.
    This class inherits from BTSQueryRequest and can be used to modify
    instances of that superclass whenever a general search request is to be
    specialized for lemma entry searches, e.g. by implementations of the
    SearchViewer interface designed for lemma entry operations.

---
+++ org.bbaw.bts.core.services.corpus/src/org/bbaw/bts/core/services/corpus/util/BTSLemmaQueryRequest.java
@@ -0,0 +1,17 @@
+package org.bbaw.bts.core.services.corpus.util;
+
+import org.bbaw.bts.core.dao.util.BTSQueryRequest;
+
+public class BTSLemmaQueryRequest extends BTSQueryRequest {
+	
+	public BTSLemmaQueryRequest(BTSQueryRequest base) {
+		
+	}
+
+	
+	@Override
+	public void initQueryBuilder() {
+		super.initQueryBuilder();
+	}
+	
+}
