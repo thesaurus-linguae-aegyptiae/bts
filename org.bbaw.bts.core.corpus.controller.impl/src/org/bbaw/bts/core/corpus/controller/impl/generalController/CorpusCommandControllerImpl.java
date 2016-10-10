ce323cee3c523f092614cdc78583a331762e5ab2
Jakob Höper <jhoeper@bbaw.de>
2016-10-10 15:02:59 +0200

    Add confirmation dialog for delete permanent command.

--- org.bbaw.bts.core.corpus.controller.impl/src/org/bbaw/bts/core/corpus/controller/impl/generalController/CorpusCommandControllerImpl.java
+++ org.bbaw.bts.core.corpus.controller.impl/src/org/bbaw/bts/core/corpus/controller/impl/generalController/CorpusCommandControllerImpl.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import org.bbaw.bts.btsmodel.BTSComment;
 import org.bbaw.bts.btsmodel.BTSDBBaseObject;
+import org.bbaw.bts.commons.BTSConstants;
 import org.bbaw.bts.core.corpus.controller.generalController.CorpusCommandController;
 import org.bbaw.bts.core.services.BTSCommentService;
 import org.bbaw.bts.core.services.corpus.CorpusObjectService;
@@ -35,7 +36,9 @@ public class CorpusCommandControllerImpl implements CorpusCommandController {
 			((BTSCorpusObject)object).setState(BTSConstants.OBJECT_STATE_TERMINATED);
 			corpusObjectService.remove((BTSCorpusObject) object);
 			return true;
-		}if (object instanceof BTSComment) {
+		}
+		if (object instanceof BTSComment) {
+			((BTSComment)object).setState(BTSConstants.OBJECT_STATE_TERMINATED);
 			commentService.remove((BTSComment) object);
 			return true;
 		}
