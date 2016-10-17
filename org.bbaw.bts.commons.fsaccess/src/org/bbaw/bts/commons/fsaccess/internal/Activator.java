1ea26b7e3441d2cc18ae03a4c439835c2451cd97
plutte <plutte@bbaw.de>
2016-10-17 11:57:48 +0200

    merging conflicts

---
+++ org.bbaw.bts.commons.fsaccess/src/org/bbaw/bts/commons/fsaccess/internal/Activator.java
@@ -0,0 +1,8 @@
+package org.bbaw.bts.commons.fsaccess.internal;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+public class Activator extends AbstractUIPlugin {
+
+	
+}
--- org.bbaw.bts.commons/src/org/bbaw/bts/commons/BTSConstants.java
+++ org.bbaw.bts.commons/src/org/bbaw/bts/commons/BTSConstants.java
@@ -1,33 +1,20 @@
 package org.bbaw.bts.commons;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Properties;
 import java.util.UUID;
 
 import javax.xml.bind.DatatypeConverter;
 
 import org.apache.commons.codec.binary.Base64;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Platform;
 
 public class BTSConstants
 {
 	private static final DecimalFormat timeFormat4 = new DecimalFormat("0000;0000");
 
-	private static Properties PROPERTIES;
-	private static String PROPERTIES_FILENAME = "btsConfig.properties";
-	public static final String BTS_HOME;
+	public static String PROPERTIES_FILENAME = "btsConfig.properties";
 
-	/** file separator. */
-	public static final String FS = System.getProperty("file.separator");
 
 	public static final String ENCODING = "UTF-8";
 	public static final String TEXT_VERS_BREAK_MARKER = "VersbreakMarker";
@@ -237,118 +224,6 @@ public class BTSConstants
 
 
 	
-	private BTSConstants()
-	{
-	}
-
-	/* Initialisierung */
-	static
-	{
-		IPath actLoc = BTSCommonsActivator.getDefault().getStateLocation();
-		IPath p = Platform.getLocation();
-
-		String home = BTSCommonsActivator.getBTSHome();
-		if (home != null)
-		{
-
-			BTS_HOME = home;
-
-		} else
-		{
-			BTS_HOME = getInstallationDir();
-//			{
-//				if (System.getProperty("os.name").toLowerCase().contains("mac"))
-//				{
-//					BTS_HOME = actLoc.removeLastSegments(7).toOSString();// + FS +
-//				} else
-//				{
-//					BTS_HOME = actLoc.removeLastSegments(4).toOSString();// + FS +
-//				}
-//			}
-		}
-		/** Properties laden. */
-		PROPERTIES = new Properties();
-		File file = new File(PROPERTIES_FILENAME);
-		// try {
-		// if (_i) _l.info("Lade: " + file.toURI().toString());
-
-		if (file.exists())
-		{
-			try
-			{
-				PROPERTIES.load(new FileInputStream(file));
-			} catch (FileNotFoundException e)
-			{
-				e.printStackTrace();
-			} catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static String getInstallationDir() {
-		URL entry = Platform.getBundle("org.bbaw.bts.commons").getEntry(
-				"/META-INF");
-		if (entry != null) {
-			String fileURL = null;
-			try {
-				fileURL = FileLocator.toFileURL(entry).getPath();
-				fileURL = fileURL.substring(1, fileURL.length());
-				File file = new File(fileURL);
-				File parent = file.getParentFile();
-				while (parent != null
-						&& !(parent.getName().equals("configuration") && !(parent
-								.getName().equals("workspace")))) {
-					parent = parent.getParentFile();
-					if (parent != null)
-					{
-						System.out.println("BTSConstants getInstallationDir: " +parent.getAbsolutePath());
-					}
-				}
-				if (parent == null) {
-					File dir = new File(System.getProperty("user.home")
-							+ BTSConstants.FS + "bts");
-					if (!dir.exists()) {
-						dir.mkdirs();
-					}
-					System.out.println("BTSConstants getInstallationDir return dir: " +dir.getAbsolutePath());
-					return dir.getAbsolutePath();
-				}
-				file = parent.getParentFile();
-				String path = file.getAbsolutePath();
-				System.out.println("BTSConstants getInstallationDir return path: " +path);
-				return path;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		}
-		String userEnvPath = System.getenv("user.home"); 
-		System.out.println("BTSConstants getInstallationDir return userEnvPath: " +userEnvPath);
-		return userEnvPath;
-	}
-
-	
-	/** Returns the Database Installation directory within the given btsInstallation directory. 
-	 * DBInstallationDir is the directory where the folder 'CouchDB' is located. 
-	 * Under an installation of CouchDB using e.g. msi-installer that would be the Programs/Apache Software...
-	 * In bts case it is the <btsInstallationDir>/dbdir
-	 * 
-	 * @param btsInsallationDir BTS Installation Directory
-	 * @return Database Installation Directory
-	 */
-	public static String getDBInstallationDir(String btsInsallationDir) {
-		if (btsInsallationDir == null)
-		{
-			btsInsallationDir = getInstallationDir();
-		}
-		String dbdir = btsInsallationDir + BTSConstants.FS + BTSConstants.DB_DIR;
-
-		return dbdir;
-	}
-
-
 	public static String getSidWithCalendar()
 	{
 		Calendar cal = Calendar.getInstance();
