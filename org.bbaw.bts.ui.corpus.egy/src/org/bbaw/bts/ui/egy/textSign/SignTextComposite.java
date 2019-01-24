thesaurus-linguae-aegyptiae/bts

commit 3a1c6b29d235895b5d35f56e4e0e0e53a08a71a9

use better approximation of ste figure widths

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/SignTextComposite.java
@@ -80,6 +80,7 @@ import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.TypedEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.PaletteData;
@@ -140,7 +141,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 	@Inject
 	@Preference(value = BTSEGYUIConstants.PREF_LEMMATIZER_FELXION_DEFAULT, nodePath = "org.bbaw.bts.ui.corpus.egy")
 	private Integer defaultFlexion;
-	
+
 	private ElementFigure selectedElement;
 
 	private Composite parentComposite;
@@ -311,7 +312,6 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 				} else if (ke.keycode == SWT.ARROW_UP) {
 					shiftLineSelection(-1);
 				}
-
 			}
 		};
 		container.setFocusTraversable(true);
@@ -322,16 +322,18 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		
 		this.layout();
 		parentComposite.layout();
-		Double d = (canvas.getViewport().getBounds().width ) * 1.35;
-		max_line_length = d.intValue();
+
+		max_line_length = canvas.getViewport().getBounds().width;
 
 	}
-	
+
+
 	@Override
 	public void addFocusListener(FocusListener listener) {
 		super.addFocusListener(listener);
 		canvas.addFocusListener(listener);
 	}
+
 
 	private void shiftLineSelection(int shift) {
 		int newLineIndex = lineIndex + shift;
@@ -578,6 +580,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 	private void loadText() {
 		purgeAll();
 		max_line_length = canvas.getViewport().getBounds().width;
+		System.out.println("maximum line length: "+max_line_length);
 		continuingRelatingObjects = new Vector<BTSObject>();
 		// canvas = new FigureCanvas(this);
 		// canvas.setBackground(COLOR_CANVAS_BACKGROUND);
@@ -620,7 +623,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 					if (senItem instanceof BTSWord) {
 						BTSWord word = (BTSWord) senItem;
 						itemFigure = makeWordFigure(word);
-						// appendWord(word);
+						appendFigure(itemFigure);
 					}
 					else if (senItem instanceof BTSMarker) {
 						BTSMarker marker = (BTSMarker) senItem;
@@ -669,6 +672,11 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		// layout.setMinorSpacing(3);
 		canvas.setContents(container);
 		// container.setLayoutManager(layout);
+		
+		System.out.println("max line length: "+max_line_length);
+		for (FontData fd : JFaceResources.getFont("BBAWLibertine").getFontData()) {
+			System.out.println(fd);
+		}
 
 		container.setFocusTraversable(true);
 		this.layout();
@@ -729,7 +737,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 				&& itemFigure instanceof WordFigure)
 		{
 			for (Object fig : itemFigure.getChildren()) {
-				if (fig instanceof TypedLabel && ((TypedLabel)fig).getType() == TypedLabel.TRANSLITATION) {
+				if (fig instanceof TypedLabel && ((TypedLabel)fig).getType() == TypedLabel.TRANSLITERATION) {
 					 ((TypedLabel)fig).setForegroundColor(BTSUIConstants.COLOR_RUBRUM);
 				}
 			}
@@ -1006,7 +1014,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 	private ElementFigure makeWordFigure(BTSWord word) {
 		TypedLabel label = new TypedLabel();
 		label.setText(word.getWChar());
-		label.setType(TypedLabel.TRANSLITATION);
+		label.setType(TypedLabel.TRANSLITERATION);
 
 		final WordFigure rect = new WordFigure(label);
 		rect.setBackgroundColor(colorWordDeselected(word));
@@ -1068,7 +1076,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 				addTransToWordFigure(word, rect, lang);
 			}
 		}
-		rect.setSize(90, 290);
+		rect.setSize(rect.calculateWidth(), 290);
 		rect.addFigureListener(new FigureListener() {
 
 			@Override
@@ -1081,7 +1089,6 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		});
 		rect.addMouseListener(elementSelectionListener);
 		rect.setLayoutManager(tl);
-		appendFigure(rect);
 
 		if (!word.eAdapters().contains(notifier)) {
 			word.eAdapters().add(notifier);
@@ -1128,9 +1135,9 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		if (currentLineFigure == null) {
 			currentLineFigure = makeLineFigure();
 		}
-		int len = calculateWordFigureLength(figure);
-		
-		System.out.println("line leng " + currentLineFigure.getSpaceLength() + len + " max " + max_line_length); 
+		int len = figure.calculateWidth() + 2;
+				
+		System.out.println("line leng " + (currentLineFigure.getSpaceLength() + len) + " max " + max_line_length); 
 		if (figure.getType().equals(ElementFigure.SENTENCE_START)) {
 			currentLineFigure = makeLineFigure();
 			currentLineFigure.add(figure);
@@ -1356,6 +1363,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 	}
 
 	private void setSelectionInternal(ElementFigure figure, int eventType) {
+		System.out.println("figure width: "+figure.calculateWidth());
 		if (figure.getParent() instanceof LineFigure) {
 			if (figure.getParent() != currentLineFigure) {
 				currentLineFigure = (LineFigure) figure.getParent();
@@ -1732,7 +1740,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 							l.setText(word.getFlexCode());
 							fset = true;
 							break;
-						case TypedLabel.TRANSLITATION :
+						case TypedLabel.TRANSLITERATION :
 							l.setText(word.getWChar());
 							break;
 						case TypedLabel.TRANSLATION :

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/support/ElementFigure.java
@@ -22,8 +22,6 @@ public interface ElementFigure extends IFigure {
 
 	String AMBIVALENCE_END = "AMBIVALENCE_END";
 
-	public int getLength();
-
 	public Object getModelObject();
 
 	public void setModelObject(Object modelObject);
@@ -37,4 +35,8 @@ public interface ElementFigure extends IFigure {
 	void addRelatingObject(BTSObject relatingObject);
 
 	List<BTSInterTextReference> getInterTextReferences();
+
+	/** returns approximate width in pixels **/
+	int calculateWidth();
+	
 }

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/support/ElementFigureImpl.java
@@ -5,8 +5,11 @@ import java.util.Vector;
 
 import org.bbaw.bts.btsmodel.BTSInterTextReference;
 import org.bbaw.bts.btsmodel.BTSObject;
+import org.eclipse.draw2d.FigureUtilities;
+import org.eclipse.draw2d.ImageFigure;
+import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.RoundedRectangle;
-import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.swt.graphics.Color;
 
 public class ElementFigureImpl extends RoundedRectangle implements
@@ -22,11 +25,7 @@ public class ElementFigureImpl extends RoundedRectangle implements
 
 	private Color baseBackgroundcolor;
 
-	public int getLength() {
-		Rectangle rec = getBounds();
-		Double d = rec.getSize().width * 0.7;
-		return d.intValue();
-	}
+
 
 	public Object getModelObject() {
 		return modelObject;
@@ -98,4 +97,36 @@ public class ElementFigureImpl extends RoundedRectangle implements
 	public Color getBaseBackgroundcolor() {
 		return baseBackgroundcolor;
 	}
+
+	@Override
+	public int calculateWidth() {
+		// tries to find the maximum width of all child elements 
+		int w = 0;
+		for (Object o : getChildren()) {
+			if (o instanceof Label) {
+				// if its a label, simulate size using default font
+				// (should have been initializes in startup controller)
+				Label l = (Label) o;
+				int labelWidth = FigureUtilities.getTextWidth(
+						l.getText(),
+						JFaceResources.getFont(
+								JFaceResources.DEFAULT_FONT));
+				if (l.getIcon() != null) {
+					labelWidth += l.getIconBounds().width + 3;
+				}
+				w = Math.max(
+						labelWidth,
+						w);
+			} else if (o instanceof ImageFigure) {
+				// if its a figure, use figure dimensions
+				ImageFigure i = (ImageFigure) o;
+				if (i.getImage() != null) {
+					int imageWidth = i.getImage().getBounds().width;
+					w = Math.max(imageWidth, w);
+				}
+			}
+		}
+		return w;
+	}
+
 }

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/support/LineFigure.java
@@ -53,11 +53,12 @@ public class LineFigure extends Figure {
 		int len = 0;
 		for (IFigure fig : children) {
 			if (fig instanceof ElementFigure) {
-				len = len + ((ElementFigure) fig).getLength();
+				len = len + ((ElementFigure) fig).calculateWidth();
 			} else {
+				System.out.println("not an elementfigure: "+fig);
 				len = len + fig.getBounds().getSize().width;
 			}
 		}
-		return len;
+		return len + (children.size() - 1) * 2;
 	}
 }

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/support/MarkerFigure.java
@@ -5,7 +5,6 @@ import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.ToolbarLayout;
 import org.eclipse.draw2d.geometry.Dimension;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.swt.graphics.Color;
 
 public class MarkerFigure extends ElementFigureImpl {
@@ -25,11 +24,10 @@ public class MarkerFigure extends ElementFigureImpl {
 		super.setCornerDimensions(new Dimension(0, 0));
 		this.name = name;
 	}
-	
-	public int getLength() {
-		
-		int len = this.name.length() * 5;
-		return len > 15 ? len : 15;
+
+
+	public String getName() {
+		return name;
 	}
-	
+
 }

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/support/TypedLabel.java
@@ -9,7 +9,7 @@ public class TypedLabel extends Label{
 	
 	public static final int FLEXION = 1<<1;
 	
-	public static final int TRANSLITATION = 1<<2;
+	public static final int TRANSLITERATION = 1<<2;
 
 	public static final int TRANSLATION = 1<<3;
 	

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/support/WordFigure.java
@@ -16,6 +16,8 @@ public class WordFigure extends ElementFigureImpl
 	private String mdc;
 	int imageWidth;
 	private ImageFigure imageFigure;
+
+
 	public WordFigure(Label name)
 	{
 		ToolbarLayout layout = new ToolbarLayout();
@@ -44,13 +46,10 @@ public class WordFigure extends ElementFigureImpl
 	}
 
 	@Override
-	public int getLength() {
-		if (imageFigure != null && imageWidth > 0) {
-			return imageWidth + 6;
-		} else {
-			return super.getLength();
-		}
+	public int calculateWidth() {
+		return super.calculateWidth() + 6;
 	}
+
 
 	public int getImageWidth() {
 		return imageWidth;
@@ -66,7 +65,6 @@ public class WordFigure extends ElementFigureImpl
 
 	public void setImageFigure(ImageFigure imageFigure) {
 		this.imageFigure = imageFigure;
-		
 	}
 
 	public ImageFigure getImageFigure() {

org.fuberlin.bts.ui.corpus.egy.annotations/src/org/fuberlin/bts/ui/corpus/egy/annotations/parts/TextAnnotationsComposite.java
@@ -1513,7 +1513,7 @@ public class TextAnnotationsComposite extends Composite implements IBTSEditor {
 				&& itemFigure instanceof WordFigure)
 		{
 			for (Object fig : itemFigure.getChildren()) {
-				if (fig instanceof TypedLabel && ((TypedLabel)fig).getType() == TypedLabel.TRANSLITATION) {
+				if (fig instanceof TypedLabel && ((TypedLabel)fig).getType() == TypedLabel.TRANSLITERATION) {
 					 ((TypedLabel)fig).setForegroundColor(BTSUIConstants.COLOR_RUBRUM);
 				}
 			}
@@ -1799,7 +1799,7 @@ public class TextAnnotationsComposite extends Composite implements IBTSEditor {
 	private ElementFigure makeWordFigure(BTSWord word) {
 		TypedLabel label = new TypedLabel();
 		label.setText(word.getWChar());
-		label.setType(TypedLabel.TRANSLITATION);
+		label.setType(TypedLabel.TRANSLITERATION);
 
 		final WordFigure rect = new WordFigure(label);
 		rect.setBackgroundColor(colorWordDeselected(word));
@@ -2278,7 +2278,7 @@ public class TextAnnotationsComposite extends Composite implements IBTSEditor {
 								l.setText(word.getFlexCode());
 								fset = true;
 								break;
-							case TypedLabel.TRANSLITATION :
+							case TypedLabel.TRANSLITERATION :
 								l.setText(word.getWChar());
 								break;
 							case TypedLabel.TRANSLATION :

commit d065b27eee4b07d06bfcef95c316273cb8ffac7b

Fix broken ambivalence rendering
Caused by an error in 3a1c6b2, words within ambivalences would not be
shown anymore.

org.bbaw.bts.app.feature/feature.xml
@@ -12,6 +12,11 @@
 Change log
 ==========
 
+Release 3.2.3
+-------------
+
+	- Fix: broken ambivalence rendering in Sign Text Editor ()
+
 Release 3.2.2
 -------------
 

org.bbaw.bts.ui.corpus.egy/src/org/bbaw/bts/ui/egy/textSign/SignTextComposite.java
@@ -615,8 +615,9 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 				BTSSenctence sentence = (BTSSenctence) item;
 
 				// insert start Sentence
-				ElementFigure sentenceStart = makeSentenceStartFigure(sentence);
-				appendFigure(sentenceStart);
+				appendFigure(
+						makeSentenceBoundsFigure(sentence, ElementFigure.SENTENCE_START)
+						);
 				for (BTSSentenceItem senItem : sentence.getSentenceItems()) {
 					ElementFigure itemFigure = null;
 					if (senItem instanceof BTSWord) {
@@ -632,7 +633,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 					}
 					else if (senItem instanceof BTSAmbivalence) {
 						BTSAmbivalence ambivalence = (BTSAmbivalence) senItem;
-						itemFigure = makeAmbivalenceFigure(ambivalence);
+						makeAmbivalenceFigures(ambivalence);
 
 					}
 					
@@ -656,8 +657,9 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 					}
 
 				}
-				ElementFigure sentenceEnd = makeSentenceEndFigure(sentence);
-				appendFigure(sentenceEnd);
+				appendFigure(
+						makeSentenceBoundsFigure(sentence, ElementFigure.SENTENCE_END)
+						);
 
 			}
 		}
@@ -762,10 +764,11 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		}
 	}
 
-	private ElementFigure makeAmbivalenceFigure(BTSAmbivalence ambivalence) {
+	private void makeAmbivalenceFigures(BTSAmbivalence ambivalence) {
 
-		ElementFigure ambivalenceStart = makeAmbivalenceStartFigure(ambivalence);
-		appendFigure(ambivalenceStart);
+		appendFigure(
+				makeAmbivalenceBoundsFigure(ambivalence, ElementFigure.AMBIVALENCE_START)
+				);
 
 		for (BTSLemmaCase lemmaCase : ambivalence.getCases()) {
 			ElementFigure caseFigure = makeCaseFigure(lemmaCase);
@@ -776,7 +779,7 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 				if (amItem instanceof BTSWord) {
 					BTSWord word = (BTSWord) amItem;
 					itemFigure = makeWordFigure(word);
-					// appendWord(word);
+					appendFigure(itemFigure);
 				} else if (amItem instanceof BTSMarker) {
 					BTSMarker marker = (BTSMarker) amItem;
 					itemFigure = makeMarkerFigure(marker);
@@ -803,13 +806,13 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 				// process relating Objects
 				}
 			}
-
 		}
-		ElementFigure ambivalenceEnd = makeAmbivalenceEndFigure(ambivalence);
-		appendFigure(ambivalenceEnd);
 
-		return null;
+		appendFigure(
+				makeAmbivalenceBoundsFigure(ambivalence, ElementFigure.AMBIVALENCE_END)
+				);
 	}
+
 
 	private ElementFigure makeCaseFigure(BTSLemmaCase lemmaCase) {
 		MarkerFigure fig = new MarkerFigure("case");
@@ -826,20 +829,16 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		return fig;
 	}
 
-	private ElementFigure makeAmbivalenceEndFigure(BTSAmbivalence ambivalence) {
-		AmbivalenceEndFigure fig = new AmbivalenceEndFigure("");
 
-		fig.setType(ElementFigure.AMBIVALENCE_END);
-		fig.setModelObject(ambivalence);
-		fig.setSize(15, 90);
-		fig.addMouseListener(elementSelectionListener);
-		return fig;
-	}
-
-	private ElementFigure makeAmbivalenceStartFigure(BTSAmbivalence ambivalence) {
+	/**
+	 * Creates an {@link ElementFigure} instance representing the beginning or the end of an ambivalence
+	 * @param ambivalence the {@link BTSAmbivalence} represented by the to-be-returned figure
+	 * @param type either {@link ElementFigure#AMBIVALENCE_START} or {@link ElementFigure#AMBIVALENCE_END}
+	 * @return
+	 */
+	private ElementFigure makeAmbivalenceBoundsFigure(BTSAmbivalence ambivalence, String type) {
 		AmbivalenceStartFigure fig = new AmbivalenceStartFigure("");
-
-		fig.setType(ElementFigure.AMBIVALENCE_START);
+		fig.setType(type);
 		fig.setModelObject(ambivalence);
 		fig.setSize(15, 90);
 		fig.addMouseListener(elementSelectionListener);
@@ -1168,19 +1167,17 @@ public class SignTextComposite extends Composite implements IBTSEditor {
 		return fig;
 	}
 
-	private ElementFigure makeSentenceStartFigure(BTSSenctence sentence) {
-		MarkerFigure fig = new MarkerFigure(" § ");
-		fig.setModelObject(sentence);
-		fig.setType(ElementFigure.SENTENCE_START);
-		fig.setSize(15, 90);
-		fig.addMouseListener(elementSelectionListener);
-		return fig;
-	}
 
-	private ElementFigure makeSentenceEndFigure(BTSSenctence sentence) {
+	/**
+	 * Creates an {@link ElementFigure} instance representing the beginning or end of a sentence. 
+	 * @param sentence
+	 * @param type either {@link ElementFigure#SENTENCE_START} or {@link ElementFigure#SENTENCE_END}
+	 * @return
+	 */
+	private ElementFigure makeSentenceBoundsFigure(BTSSenctence sentence, String type) {
 		MarkerFigure fig = new MarkerFigure(" § ");
 		fig.setModelObject(sentence);
-		fig.setType(ElementFigure.SENTENCE_END);
+		fig.setType(type);
 		fig.setSize(15, 90);
 		fig.addMouseListener(elementSelectionListener);
 		return fig;